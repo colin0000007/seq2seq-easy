f761130d8a4c4b6279c4787534af2c84bbb23f05
outsider <2496558529@qq.com>
2019-05-04 17:25:31 +0800

    Update Seq2SeqModel.py
    修正一些错误

--- Myseq2seq/Seq2SeqModel.py
+++ Myseq2seq/Seq2SeqModel.py
@@ -1,8 +1,8 @@
 #encoding:UTF-8
 import tensorflow as tf
 from tensorflow.python.keras.layers.core import Dense
-from Myseq2seq.dataPreprocessing import batch_generator
-from Myseq2seq.dataPreprocessing import source_list,target_list,source_seq_list,target_seq_list,target_max_len,source_max_len,target_token_2_id
+from seq2seqV1.dataPreprocessing import batch_generator
+from seq2seqV1.dataPreprocessing import source_list,target_list,source_seq_list,target_seq_list,target_max_len,source_max_len,target_token_2_id
 import numpy as np
 '''
 所以数据直接从dataPreprocessing中导入
@@ -11,23 +11,22 @@ pad = "<pad>"
 start_token = "<s>"
 end_token = "</s>"
 #定义encoder的输入tensor，加入了词嵌入
-def input_tensor(batch_size,source_time_step,target_time_step,source_vocab_size,source_embedding_size,target_vocab_size,target_embedding_size):
-    #[time_step,batch_size]这样是为了实则设置time_major=true，这样可以使得效率提高
-    #这里设置位None，意思时不指定batch_size用到多少就是多少？还不确定
+def input_tensor(source_time_step,target_time_step,source_vocab_size,source_embedding_size,target_vocab_size,target_embedding_size):
+    #这里设置位None，意思是不指定batch_size用到多少就是多少
     source_batch = tf.placeholder(tf.int32,[None,source_time_step],name="source_batch")
     #对source做词嵌入，词向量矩阵的shape为[source的词库大小,嵌入维度]
-    #矩阵中每一行就是一个词向量
+    #嵌入矩阵中每一行就是一个词向量
     source_embedding = tf.get_variable(shape=[source_vocab_size,source_embedding_size],name='source_embedding')
     #使用embedding_lookup从embedding矩阵中查询词向量从而将X的每一个单词的index转换一个词向量
     embedded_source_batch = tf.nn.embedding_lookup(source_embedding,source_batch)
     #最后返回的embedded_X.shape = [time_step,batch_size,embedding_size]
     #target类似
-    target_batch_x = tf.placeholder(tf.int32,[batch_size,target_time_step])
-    target_batch_y = tf.placeholder(tf.int32,[batch_size,target_time_step])
+    target_batch_x = tf.placeholder(tf.int32,[None,target_time_step])
+    target_batch_y = tf.placeholder(tf.int32,[None,target_time_step])
     target_embedding = tf.get_variable(shape=[target_vocab_size,source_embedding_size],name='target_embedding')
     embedded_target_batch_x = tf.nn.embedding_lookup(target_embedding,target_batch_x)
     source_batch_seq_len = tf.placeholder(tf.int32,[None],name="source_batch_seq_len")
-    target_batch_seq_len = tf.placeholder(tf.int32,[batch_size],name="target_batch_seq_len")
+    target_batch_seq_len = tf.placeholder(tf.int32,[None],name="target_batch_seq_len")
     return embedded_source_batch,embedded_target_batch_x,source_batch,target_batch_x,target_batch_y,source_batch_seq_len,target_batch_seq_len,target_embedding
 
 #参照tensorflow/nmt的官方教程
@@ -39,13 +38,13 @@ def build_encoder(embedded_source_batch,source_batch_seq_len,rnn_layer_size=2,rn
     rnn_layer = tf.nn.rnn_cell.MultiRNNCell([get_rnn_cell() for _ in range(rnn_layer_size)])
     #将rnn沿时间序列展开
     #   encoder_outputs: [batch_size,max_time, num_units]
-    #   encoder_state: [batch_size, num_units]
+    #   encoder_state: cell个数*[batch_size, num_units]
     #   sequence_length:传入一个list保存每个样本的序列的真实长度，教程中说这样做有助于提高效率
     encoder_outputs, encoder_state = tf.nn.dynamic_rnn(
     rnn_layer, embedded_source_batch,
     sequence_length=source_batch_seq_len, time_major=False,dtype=tf.float32)
-    #注意返回的encoder_outputs返回了每一个序列节点的output shape为[max_time, batch_size, num_units]
-    #返回的encoder_state返回了每个样本最后一个序列节点的隐状态/cell值，猜测是这样，是否是待定
+    #返回的encoder_outputs返回了每一个序列节点的output shape为[max_time, batch_size, num_units]
+    #返回的encoder_state返回了每个cell最后一个序列节点的state输出
     return encoder_outputs, encoder_state
 
 '''
@@ -77,7 +76,7 @@ def build_decoder(encoder_outputs, encoder_state,embedded_target_batch_x,target_
     #with tf.variable_scope("decode", reuse=True):
         #按照官方GreedyEmbeddingHelper的文档，需要传入一个shape为batchsize大小的start_token，类型为int32
         #好像这里的batch size就决定了预测时的batch 大小，必须符合才行
-    #原文中的inference部分已经被inference函数替换，强烈不建议使用
+    #原文中的inference部分已经被inference函数替换
     '''
     start_tokens = tf.constant(start_token_id, dtype=tf.int32, shape=[batch_size], name='start_tokens')
     predicting_helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(target_embedding,start_tokens,end_token_id)
@@ -102,12 +101,12 @@ def inference(start_token_id,target_embedding,end_token_id,encoder_state,decoder
     
 def seq2seq_model(embedded_source_batch,source_batch_seq_len,embedded_target_batch_x,target_batch_seq_len,target_embedding,encoder_rnn_layer_size=2,encoder_rnn_num_units=128,decoder_rnn_layer_size=2,decoder_rnn_num_units=128):
     encoder_outputs, encoder_state = build_encoder(embedded_source_batch, source_batch_seq_len, encoder_rnn_layer_size, encoder_rnn_num_units)
-    decoder_output,predicting_decoder_output,rnn_layer,projection_layer = build_decoder(encoder_outputs, encoder_state, embedded_target_batch_x, target_batch_seq_len, target_embedding, decoder_rnn_layer_size, decoder_rnn_num_units)
+    decoder_output,rnn_layer,projection_layer = build_decoder(encoder_outputs, encoder_state, embedded_target_batch_x, target_batch_seq_len, target_embedding, decoder_rnn_layer_size, decoder_rnn_num_units)
+    inference(target_token_2_id[start_token], target_embedding, target_token_2_id[end_token], encoder_state, rnn_layer, projection_layer, target_max_len)
     print("encoder_outputs:",encoder_outputs)
     print("encoder_state:",encoder_state)
     print("decoder_output:",decoder_output)
-    print("predicting_decoder_output",predicting_decoder_output)
-    return decoder_output,predicting_decoder_output,rnn_layer,projection_layer,encoder_state
+    return decoder_output
 
 #一系列超参数
 lr = 0.001
@@ -128,17 +127,14 @@ def build_graph():
     train_graph = tf.Graph()
     with train_graph.as_default():
         #1.tensor声明
-        embedded_source_batch,embedded_target_batch_x,source_batch,target_batch_x,target_batch_y,source_batch_seq_len,target_batch_seq_len,target_embedding = input_tensor(batch_size, source_max_len, target_max_len, source_dic_size, source_embedding_size, target_dic_size, target_embedding_size)
+        embedded_source_batch,embedded_target_batch_x,source_batch,target_batch_x,target_batch_y,source_batch_seq_len,target_batch_seq_len,target_embedding = input_tensor(source_max_len, target_max_len, source_dic_size, source_embedding_size, target_dic_size, target_embedding_size)
         #2.构造seq2seq产生的output tensor
-        decoder_output,rnn_layer,projection_layer,encoder_state = seq2seq_model(embedded_source_batch, source_batch_seq_len, embedded_target_batch_x, target_batch_seq_len, target_embedding, encoder_rnn_layer_size, encoder_rnn_num_units, decoder_rnn_layer_size, decoder_rnn_num_units)
+        decoder_output = seq2seq_model(embedded_source_batch, source_batch_seq_len, embedded_target_batch_x, target_batch_seq_len, target_embedding, encoder_rnn_layer_size, encoder_rnn_num_units, decoder_rnn_layer_size, decoder_rnn_num_units)
         #1和2这个步骤必须在同一个graph下声明
         #对这2个decoder的输出获取不同的tensor并且取名字
         training_logits = tf.identity(decoder_output.rnn_output, 'logits')
-        #predicting_logits = tf.identity(predicting_decoder_output.sample_id, name='predictions')
-        print("predicting_logits.shape:",predicting_logits.shape)
         print("training_logits.shape:",training_logits.shape)
         #尝试是否能成功
-        inference(1, target_embedding, 2, encoder_state, rnn_layer, projection_layer, target_max_len)
         #mask的作用是：计算loss时忽略pad的部分，这部分的loss不需要算，提升性能，
         masks = tf.sequence_mask(target_batch_seq_len, target_max_len, dtype=tf.float32, name='masks')
         with tf.name_scope("optimization"):
